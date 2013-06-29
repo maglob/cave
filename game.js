04a29166b9ec70e0fde73e2f960e48c7e07d01e6
Marko Aalto <marko.aalto@reaktor.fi>
2013-06-29 23:21:00 +0300

    Refactor and cleanup

--- game.js
+++ game.js
@@ -50,9 +50,9 @@ function set(elementIdDotAttribute, value) {
 function Grid(minCorner, maxCorner, cellSize, fnInitCell) {
   this.cells = []
   this.cellSize = cellSize
-  this.cellsPerRow = Math.ceil((maxCorner[0]-minCorner[0]) / cellSize)
-  this.minCorner = minCorner
-  this.maxCorner = maxCorner
+  this.minCorner = minCorner.sub(cellSize)
+  this.maxCorner = maxCorner.add(cellSize)
+  this.cellsPerRow = Math.ceil((this.maxCorner[0]-this.minCorner[0]) / cellSize)
   this.fnInitCell = fnInitCell ? fnInitCell : function() { return [] }
 }
 
@@ -71,16 +71,21 @@ Grid.prototype.getCell = function (p) {
     if (!r) 
       r = this.cells[id] = this.fnInitCell(this.toGridCoord(p).scale(this.cellSize).add(this.minCorner), this.cellSize)
     return r
-  }
+  } else
+    return []
+}
+
+function createSvgElement(name) {
+  return document.createElementNS("http://www.w3.org/2000/svg", name)
 }
 
 function Bullet(pos, v) {
   this.pos = pos.clone()
   this.v = v.clone()
-  this.dom = document.getElementById("bullets").appendChild(
-    document.createElementNS("http://www.w3.org/2000/svg", "rect"))
+  this.dom = spritesDom.appendChild(createSvgElement("rect"))
   this.dom.setAttribute("width", 2)
   this.dom.setAttribute("height", 2)
+  this.dom.setAttribute("stroke", "white")
 }
 
 function Asteroid(pos, v, a, av) {
@@ -88,12 +93,14 @@ function Asteroid(pos, v, a, av) {
   this.v = v.clone()
   this.a = a
   this.av = av
-  this.dom = document.getElementById("asteroids").appendChild(
-    document.createElementNS("http://www.w3.org/2000/svg", "polygon"))
+  this.dom = spritesDom.appendChild(createSvgElement("polygon"))
   this.dom.setAttribute("points", asteroidPoints)
+  this.dom.setAttribute("stroke", "lightgray")
+  this.dom.setAttribute("fill", "gray")
 }
 
 function init() {
+  spritesDom = document.getElementById("sprites")
   set('ship.points', shipPoints)
   set('cave.points', cavePoints)
   set('shield.r', 26)
@@ -104,6 +111,17 @@ function init() {
                                 createUnit(Math.random()*Math.PI*2).scale(2+Math.random()*2),
                                 Math.random()*Math.PI*2, .5-Math.random()*1))
   tick()
+}
+
+function cleanupSprites(sprites) {
+  sprites.filter(function (s) { 
+    return s.collision 
+  }).forEach(function (s) {
+    spritesDom.removeChild(s.dom)
+  })
+  return sprites.filter(function (s) {
+    return !s.collision
+  })
 }
 
 function updateWorld() {
@@ -117,51 +135,36 @@ function updateWorld() {
     bullets.push(new Bullet(ship.pos, createUnit(ship.a).scale(10).add(ship.v)));
   ship.v.add(gravity, ship.v)
   ship.pos.add(ship.v, ship.pos)
-  for(var i=0; i<bullets.length; i++) {
-    var b = bullets[i]
-    b.pos.add(b.v, b.pos)
-    var lines = grid.getCell(b.pos)
-    if (lines)
-      for(var j=0; j<lines.length; j++) 
-        b.collision |= lines[j].intersects(b.pos.sub(b.v), b.pos)
-    else
-      b.collision = true
-  }
-  for(var i=0; i<bullets.length; i++)
-    if(bullets[i].collision) {
-      bullets[i].dom.parentNode.removeChild(bullets[i].dom)
-      bullets.splice(i--, 1)
-    }
-  for(var i=0; i<asteroids.length; i++) {
-    var a = asteroids[i]
-    a.pos.add(a.v, a.pos)
-    a.a += a.av
-    var lines = grid.getCell(a.pos)
-    if (lines)
-      for(var j=0; j<lines.length; j++) 
-        for(var k=0; k<asteroidLines.length; k++) {
-          var p1 = asteroidLines[k].a.rotate(a.a).add(a.pos)
-          var p2 = asteroidLines[k].b.rotate(a.a).add(a.pos)
-          a.collision |= lines[j].intersects(p1, p2)
-        }
-    else
-      a.collision = true
-  }
-  for(var i=0; i<asteroids.length; i++) 
-    if(asteroids[i].collision) {
-      asteroids[i].dom.parentNode.removeChild(asteroids[i].dom)
-      asteroids.splice(i--, 1)
-    }
   ship.controls.shield = false
   var lines = grid.getCell(ship.pos)
-  if (lines) {
-    for(var i=0; i<lines.length; i++) {
-      var l = lines[i]
-      var rotPoints = shipPoints.rotate(ship.a).add(ship.pos)
-      for(var j=0; j<rotPoints.length/2; j++) 
-        ship.controls.shield |= l.intersects(rotPoints.point(j), rotPoints.point(j+1))
-    }
+  for(var i=0; i<lines.length; i++) {
+    var l = lines[i]
+    var rotPoints = shipPoints.rotate(ship.a).add(ship.pos)
+    for(var j=0; j<rotPoints.length/2-1; j++) 
+      ship.controls.shield |= l.intersects(rotPoints.point(j), rotPoints.point(j+1))
   }
+
+  bullets.forEach(function (b) {
+    b.pos.add(b.v, b.pos)
+    grid.getCell(b.pos).forEach(function (line) {
+      b.collision |= line.intersects(b.pos.sub(b.v), b.pos)
+    })
+  })
+
+  asteroids.forEach(function (a) {
+    a.pos.add(a.v, a.pos)
+    a.a += a.av
+    grid.getCell(a.pos).forEach(function (line) {
+      for(var k=0; k<asteroidLines.length; k++) {
+        var p1 = asteroidLines[k].a.rotate(a.a).add(a.pos)
+        var p2 = asteroidLines[k].b.rotate(a.a).add(a.pos)
+        a.collision |= line.intersects(p1, p2)
+      }
+    })
+  })
+
+  bullets = cleanupSprites(bullets)
+  asteroids = cleanupSprites(asteroids)
 }
 
 function renderView() {
